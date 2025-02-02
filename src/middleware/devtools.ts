78ee31ecef3326730fd8b00dc7c47f8e0ea31fc1
Ali Mert Çakar <mail@alimert.net>
2025-02-02 10:33:39 +0300

    wip
    wip
    
    wip

--- src/middleware/devtools.ts
+++ src/middleware/devtools.ts
@@ -73,7 +73,6 @@ export interface DevtoolsOptions extends Config {
   name?: string
   enabled?: boolean
   anonymousActionType?: string
-  inferActionName?: boolean
   store?: string
 }
 
@@ -158,8 +157,7 @@ const extractConnectionInformation = (
 const devtoolsImpl: DevtoolsImpl =
   (fn, devtoolsOptions = {}) =>
   (set, get, api) => {
-    const { enabled, anonymousActionType, inferActionName, store, ...options } =
-      devtoolsOptions
+    const { enabled, anonymousActionType, store, ...options } = devtoolsOptions
 
     type S = ReturnType<typeof fn> & {
       [store: string]: ReturnType<typeof fn>
@@ -188,10 +186,8 @@ const devtoolsImpl: DevtoolsImpl =
     ;(api.setState as any) = ((state, replace, nameOrAction: Action) => {
       const r = set(state, replace as any)
       if (!isRecording) return r
-      let defaultActionName = anonymousActionType
-      if (inferActionName) {
-        defaultActionName = findCallerName(new Error().stack ?? '')
-      }
+      const defaultActionName =
+        anonymousActionType ?? findCallerName(new Error().stack ?? '')
       const action: { type: string } =
         nameOrAction === undefined
           ? { type: defaultActionName || 'anonymous' }
